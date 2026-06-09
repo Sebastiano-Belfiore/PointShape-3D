26969722389c32299194be946615049c823c8ef3
Sebastiano-Belfiore <belfioresebastiano07@gmail.com>
2026-06-09 16:30:01 +0200

    Add cube creation

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <math.h>
 #define PI 3.14159265358979323846
-#define NUMPOINTS 1000
+#define NUMPOINTS 500
 typedef struct  {
     int r,g,b;
 }Color;
@@ -10,7 +10,14 @@ typedef struct {
     float x, y, z;
 } Vertex;
 
+
+// r1, r2 sono i pesi del Point rispetto al Triangle
+// Indicano dove si trova il Point all'interno del Triangle
+// Per animare il modello, invece di rigenerare i punti con randomPointInTriangle(),
+// si usa recalc_point() che ricalcola la posizione del Point
+// seguendo i vertici del Triangle aggiornati.
 typedef struct {
+    int r1,r2;
     Vertex v;
     Color c;    
 } Point;
@@ -41,19 +48,80 @@ Vertex randomPointInTriangle(Vertex A,Vertex B,Vertex C) {
     P.z = A.z + r1 * (B.z - A.z) + r2 * (C.z - A.z);
     return P;
 }
-void setPointsToModel(Model *model){
-    int tri = model->num_triangles;
-    for(int j = 0; j < tri; j++){
-        for(int i = 0; i < NUMPOINTS; i++){
+void setPointsToModel(Model *model) {
+    for (int j = 0; j < model->num_triangles; j++) {
+        for (int i = 0; i < NUMPOINTS; i++) {
+            float r1 = (float)rand() / RAND_MAX;
+            float r2 = (float)rand() / RAND_MAX;
+            if (r1 + r2 > 1) { r1 = 1 - r1; r2 = 1 - r2; }
+
+            model->triangles[j].points[i].r1 = r1;
+            model->triangles[j].points[i].r2 = r2;
+
             Vertex A = model->vertices[model->triangles[j].v0];
             Vertex B = model->vertices[model->triangles[j].v1];
             Vertex C = model->vertices[model->triangles[j].v2];
-            model->triangles[j].points[i].v = randomPointInTriangle(A,B,C);
-            model->triangles[j].points[i].c = (Color){255,255,255};
-        }    
+            model->triangles[j].points[i].v = randomPointInTriangle(A, B, C);
+            model->triangles[j].points[i].c = (Color){255, 255, 255};
+        }
     }
 }
+void create_cube(Model *model){
+    model->num_vertices = 8;
+    model->num_triangles = 12; //6 faces
+    float h = 4;
+    float half_h = h /2; 
+    //center 0,0,0
+    //half_h distanza dal centro verso ogni vertice
+    model->vertices = malloc(sizeof(Vertex) * model->num_vertices);
+    model->triangles = malloc(sizeof(Triangle) * model->num_triangles);
+    //z -- front(+half_h), back(-half_h)
+    //x -- left(-half_h), right(+half_h)
+    //y -- up(+half_h), bottom(-half_h)
+    model->vertices[0] = (Vertex){-half_h,+half_h,+half_h};  //left-up-front -A 
+    model->vertices[1] = (Vertex){+half_h,+half_h,+half_h};  //right-up-front - B
+    model->vertices[2] = (Vertex){+half_h,-half_h,+half_h}; //right-bottom-front - C
+    model->vertices[3] = (Vertex){-half_h,-half_h,+half_h};  //left-bottom-front - D
+    model->vertices[4] = (Vertex){-half_h,+half_h,-half_h}; //left-up-back - E
+    model->vertices[5] = (Vertex){+half_h,+half_h,-half_h}; //right-up-back - F
+    model->vertices[6] = (Vertex){+half_h,-half_h,-half_h}; //right-bottom-back - G
+    model->vertices[7] = (Vertex){-half_h,-half_h,-half_h}; //left-bottom-back - H
 
+
+    //front
+    model->triangles[0] = (Triangle){0,1,2}; //A-B-C
+    model->triangles[1] = (Triangle){0,2,3}; // A-C-D
+
+    //back
+
+    model->triangles[2] = (Triangle){4,5,6}; //E-F-G
+    model->triangles[3] = (Triangle){4,6,7}; // E-G-H
+
+    //left
+
+    model->triangles[4] = (Triangle){0,4,7}; //A-E-H
+    model->triangles[5] = (Triangle){0,7,3}; //A-H-D
+
+    //right
+
+    model->triangles[6] = (Triangle){1,5,6}; //B-F-G
+    model->triangles[7] = (Triangle){1,6,2}; //B-G-C
+
+    //up
+
+    model->triangles[8] = (Triangle){0,4,5}; //A-E-F
+    model->triangles[9] = (Triangle){0,5,1}; //A-F-B
+
+    //down
+
+    model->triangles[10] = (Triangle){3,7,6}; //D-H-G
+    model->triangles[11] = (Triangle){3,6,2}; //D-G-C
+
+    setPointsToModel(model);
+
+
+    
+}
 void create_circle(Model *model, int segments){
     
     float step = (2 * PI) / segments;
@@ -66,7 +134,7 @@ void create_circle(Model *model, int segments){
         float angle = step * i;
         
         model->vertices[i] = (Vertex){
-            cosf(angle) * radius, //Togliere radius da qui e creare modello cerchio 
+            cosf(angle) * radius, 
             sinf(angle) * radius,
             0
         };
@@ -143,9 +211,9 @@ void draw(SDL_Surface *surface, float time, Model *model) {
     clear(surface);
     for (int t = 0; t < model->num_triangles; t++){
         for (int j = 0; j < NUMPOINTS; j++) {
-            float zfactor = 1 + model->triangles[t].points[j].v.z;
-            float x = (model->triangles[t].points[j].v.x / zfactor) * 50;
-            float y = (model->triangles[t].points[j].v.y / zfactor) * 50;
+            float zfactor = 1 + model->triangles[t].points[j].v.z+3;
+            float x = (model->triangles[t].points[j].v.x / zfactor) * 100;
+            float y = (model->triangles[t].points[j].v.y / zfactor) * 100;
             pixel(surface, round(cx+x), round(cy+y), 255, 255, 255);
         }
 }
@@ -186,7 +254,8 @@ int main(int argc, char* argv[]) {
 
     clear(surface);
     Model *model = malloc(sizeof(Model));
-    create_circle(model,20);
+    //reate_circle(model,20);
+    create_cube(model);
     int running = 1;
     float time = 0;
     while(running) {
